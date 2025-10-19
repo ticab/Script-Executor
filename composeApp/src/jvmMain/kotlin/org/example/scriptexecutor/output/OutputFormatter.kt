972120e31fd361ae1543f47727d2a2f1b3f38429
ticab <tija.brkovic@gmail.com>
2025-10-20 01:48:04 +0200

    Added runtime error link

--- composeApp/src/jvmMain/kotlin/org/example/scriptexecutor/output/OutputFormatter.kt
+++ composeApp/src/jvmMain/kotlin/org/example/scriptexecutor/output/OutputFormatter.kt
@@ -9,16 +9,18 @@ fun formatOutput(add: String, output: AnnotatedString, onClick: (line: Int, colu
     val res = buildAnnotatedString {
         append(output)
 
-        val compileError = Regex(""".*[/\\]\w+\.kts:(\d+):(\d+)""")
+        val compileErrorRegex = Regex(""".*[/\\]\w+\.kts:(\d+):(\d+)""")
+        val runtimeErrorRegex = Regex("""at\s+Foo(\d+)\.<init>\(foo\d+.*\.kts:(\d+)\)""")
 
         for ((index, line) in add.lines().withIndex()) {
-            val match = compileError.find(line)
+            val compileError = compileErrorRegex.find(line)
+            val runtimeError = runtimeErrorRegex.find(line)
 
-            if (match != null) {
-                val beforeText = line.take(match.range.first)
+            if (compileError != null) {
+                val beforeText = line.take(compileError.range.first)
                 append(beforeText)
 
-                val linkText = "script:${match.groupValues[1]}:${match.groupValues[2]}"
+                val linkText = "script:${compileError.groupValues[1]}:${compileError.groupValues[2]}"
                 val startIndex = length
 
                 withStyle(style = SpanStyle(color = MyColors.Blue, textDecoration = TextDecoration.Underline)) {
@@ -35,8 +37,8 @@ fun formatOutput(add: String, output: AnnotatedString, onClick: (line: Int, colu
                             )
                         ),
                         linkInteractionListener = {
-                            val line = match.groupValues[1].toInt()
-                            val column = match.groupValues[2].toInt()
+                            val line = compileError.groupValues[1].toInt()
+                            val column = compileError.groupValues[2].toInt()
                             onClick(line, column)
                         }
                     ),
@@ -44,9 +46,38 @@ fun formatOutput(add: String, output: AnnotatedString, onClick: (line: Int, colu
                     end = endIndex
                 )
 
-                val afterText = line.substring(match.range.last + 1)
+                val afterText = line.substring(compileError.range.last + 1)
                 append(afterText)
             }
+            else if (runtimeError != null) {
+                append("at script.<init>(")
+                var linkText = "script.kts:${runtimeError.groupValues[2]}"
+
+                val startIndex = length
+                withStyle(style = SpanStyle(color = MyColors.Blue, textDecoration = TextDecoration.Underline)) {
+                    append(linkText)
+                }
+                val endIndex = length
+                addLink(
+                    LinkAnnotation.Clickable(
+                        tag = "SCRIPT",
+                        styles = TextLinkStyles(
+                            style = SpanStyle(
+                                color = MyColors.Blue,
+                                textDecoration = TextDecoration.Underline
+                            )
+                        ),
+                        linkInteractionListener = {
+                            val lineNum = runtimeError.groupValues[2].toInt()
+                            onClick(lineNum, 1)
+                        }
+                    ),
+                    start = startIndex,
+                    end = endIndex
+                )
+                append(")")
+            }
+
             else{
                 append(line)
             }
